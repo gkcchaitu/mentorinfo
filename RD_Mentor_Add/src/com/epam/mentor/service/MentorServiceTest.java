a10d620de214dac2c0d38121ecf10cc5ee6366fe
Krishnachaitanya_Gog <Krishnachaitanya_Gog@EPINHYDW1281.princeton.epam.com>
2019-05-08 15:49:42 +0530

    version1 with test

---
+++ RD_Mentor_Add/src/com/epam/mentor/service/MentorServiceTest.java
@@ -0,0 +1,39 @@
+package com.epam.mentor.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.epam.mentor.bean.MentorBean;
+
+class MentorServiceTest {
+
+	static MentorService ms;
+	@BeforeAll
+	static void instantiation()
+	{
+		ms = new MentorService();
+	}
+	
+	@Test
+	void test() throws ParseException, SQLException {
+		String s="12-11-2019";
+		SimpleDateFormat sdf=new SimpleDateFormat("MM-DD-YYYY");
+		Date d=sdf.parse(s);
+		MentorBean mb = new MentorBean();
+		mb.setName("chaitu");
+		mb.setEmail(null);
+		mb.setMaxNoOfMentees(5); 
+		mb.setMentorEndDate(d);
+		mb.setMentorStartDate(d);
+		mb.setStatus("Active");
+		mb.setTechnologyStream("cse");
+		assertEquals("success",ms.addMentor(mb));
+	}
+}
---
+++ RD_Mentor_Add/src/com/epam/mentor/util/Provider.java
@@ -0,0 +1,10 @@
+package com.epam.mentor.util;
+
+public interface Provider {
+
+	String DRIVER="com.mysql.cj.jdbc.Driver";  
+	String CONNECTION_URL="jdbc:mysql://localhost:3306/RD_ADD_MENTOR";
+	String USERNAME="root";  
+	String PASSWORD="root";  
+	
+}
